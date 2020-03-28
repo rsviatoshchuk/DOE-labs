1c793203cd38dbd2e682dc0f3af27125b621c234
Rostyslav Sviatoshchuk <svyatoschuk@gmail.com>
2020-03-28 14:42:51 +0200

    5-level part, quadratic part and l-func added

--- autoexperiment.py
+++ autoexperiment.py
@@ -1,4 +1,5 @@
 import numpy
+from math import sqrt
 from scipy.stats import t, f
 
 
@@ -6,7 +7,7 @@ class Experiment:
     def __init__(self):
         self.normalized_matrix = None
 
-    def get_normalized_matrix(self, num_of_factors):
+    def get_2level_normalized_matrix(self, num_of_factors):
         def conv(str_el):
             if str_el == "0":
                 str_el = "-1"
@@ -18,6 +19,37 @@ class Experiment:
         str_array = numpy.array([list(form.format(i)[::-1]) for i in range(num_of_levels ** num_of_factors)])
         return vconv(str_array)
 
+    def get_5level_part(self, num_of_factors, l):
+        star_dots_matrix = []
+
+        for col in range(num_of_factors):
+            row = [0]*num_of_factors
+
+            row[col] = -l
+            star_dots_matrix.append(row[:])
+            row[col] = l
+            star_dots_matrix.append(row[:])
+
+        return numpy.array(star_dots_matrix)
+
+    def get_interaction_part(self, num_of_factors):
+        pass
+
+    def get_quadratic_part(self, num_of_factors, l):
+        quadratic_matrix = numpy.append(self.get_2level_normalized_matrix(num_of_factors),
+                                        self.get_5level_part(num_of_factors, l), axis=0)
+
+        return quadratic_matrix**2
+
+    def get_l_central(self, k, p):
+        return sqrt(sqrt((2**(k-p-2))*(2**(k-p)+2*k+1))-2**(k-p-1))
+    
+    def get_l_rototable(self, k):
+        return sqrt(k)
+
 
 a = Experiment()
-print(a.get_normalized_matrix(3))
+#print(a.get_2level_normalized_matrix(3))
+# print(a.get_5level_part(5, 1.44))
+print(a.get_quadratic_part(3, 1.73))
+print(a.get_l_central(3, 0))
