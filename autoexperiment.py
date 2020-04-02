6d0f0b23cf107e9141ff277d2461dff03c22e04f
Rostyslav Sviatoshchuk <svyatoschuk@gmail.com>
2020-04-02 18:56:10 +0300

    norm_matrix renamed to main-part, norm_matrix - is now ready plan of exp

--- autoexperiment.py
+++ autoexperiment.py
@@ -1,7 +1,7 @@
 import numpy
 from math import sqrt
 from scipy.stats import t, f
-
+from itertools import combinations
 
 class Experiment:
     def __init__(self):
@@ -13,15 +13,18 @@ class Experiment:
         self.factors_range = None
         self.resp_var_range = None
 
+        self.interaction_combinations = None
+
         # Флаги
         self.five_level_flag = None
         self.interaction_flag = None
         self.quadr_flag = None
 
         # Нормалізовані частини
-        self.norm_matrix = None
+        self.main_part = None
         self.interaction_part = None
         self.quadr_part = None
+        self.norm_matrix = None
 
         # Натуралізовані частини
         self.nat_matrix = None
@@ -43,9 +46,9 @@ class Experiment:
         self.interaction_flag = interaction
         self.quadr_flag = quadratic
 
-    def set_norm_matr(self, matrix):
+    def set_main_part(self, matrix):
         """func for manual norm matrix"""
-        self.norm_matrix = matrix
+        self.main_part = matrix
 
     def set_response_variable_matrix(self, matrix):
         self.resp_var_matrix = matrix
@@ -53,9 +56,9 @@ class Experiment:
     def gen_random_response_var(self, m):
         self.resp_var_matrix = numpy.random.uniform(low=self.resp_var_range[0],
                                                     high=self.resp_var_range[1],
-                                                    size=(len(self.norm_matrix), m))
+                                                    size=(len(self.main_part), m))
 
-    def gen_norm_matr(self):
+    def gen_main_part(self):
         """func generate normalized matrix"""
         def conv(str_el):
             if str_el == "0":
@@ -81,13 +84,27 @@ class Experiment:
 
             num_array = numpy.append(num_array, numpy.array(five_level_part), axis=0)
 
-        self.norm_matrix = num_array
+        self.main_part = num_array
 
     def gen_interaction_part(self):
         """func generate interaction part(only for 2 and 3 factors)"""
+        comb = []
+        for n in range(2, self.factors + 1):
+            comb.append([combinations(range(1, self.factors + 1), n)])
+        self.interaction_combinations = numpy.array(comb)
+
         if self.factors == 2:
             matrix = numpy.array([[+1], [+1], [-1], [-1]])
         elif self.factors == 3:
+            # matrix = numpy.array([[[+1, +1, +1],
+            #                       [+1, -1, -1],
+            #                       [-1, +1, -1],
+            #                       [-1, -1, +1],
+            #                       [-1, -1, +1],
+            #                       [-1, +1, -1],
+            #                       [+1, -1, -1],
+            #                       [+1, +1, +1]],
+            #                      [[-1,], [+1,], [+1,], [-1,], [+1,], [-1,], [-1,], [+1,]]])
             matrix = numpy.array([[+1, +1, +1, -1],
                                   [+1, -1, -1, +1],
                                   [-1, +1, -1, +1],
@@ -99,22 +116,42 @@ class Experiment:
         else:
             raise ValueError
 
-        self.interaction_part = matrix
+        self.interaction_part = matrix[:2**(self.factors-self.fractionality), :]
 
     def gen_quadratic_part(self):
-        self.quadr_part = self.norm_matrix**2
+        self.quadr_part = self.main_part**2
 
     def naturalize(self):
         average_x = self.factors_range.mean(axis=1)
         delta_x = average_x - self.factors_range[:, 0]
 
-        self.nat_matrix = self.norm_matrix * delta_x + average_x
+        self.nat_matrix = self.main_part * delta_x + average_x
 
         # if self.interaction_flag:
         #     self.nat_interaction_part =
+
         if self.quadr_flag:
             self.nat_quadr_part = self.nat_matrix ** 2
 
+    def gen_norm_matrix(self):
+
+        if self.interaction_flag:
+            if self.main_part.shape[0] == self.interaction_part.shape[0]:
+                matrix = numpy.append(self.main_part, self.interaction_part, axis=1)
+            else:
+                inter_matrix = numpy.append(self.interaction_part,
+                                            numpy.zeros((self.main_part.shape[0] - self.interaction_part.shape[0],
+                                                         self.interaction_part.shape[1])),
+                                            axis=0)
+                matrix = numpy.append(self.main_part, inter_matrix, axis=1)
+            if self.quadr_flag:
+                self.norm_matrix = numpy.append(matrix, self.quadr_part, axis=1)
+                return
+            else:
+                self.norm_matrix = matrix
+                return
+        if self.quadr_flag:
+            self.norm_matrix = numpy.append(self.main_part, self.quadr_part, axis=1)
 
     def get_l_central(self, k, p):
         return sqrt(sqrt((2 ** (k - p - 2)) * (2 ** (k - p) + 2 * k + 1)) - 2 ** (k - p - 1))
@@ -125,20 +162,26 @@ class Experiment:
 
 a = Experiment()
 a.set_experiment(3, [(-5, 15), (10, 60), (10, 20)], (205, 231.666), interaction=True, quadratic=True, fivelevel=True)
-a.gen_norm_matr()
+a.gen_main_part()
 a.gen_interaction_part()
 a.gen_quadratic_part()
+print(a.main_part.shape)
+print(a.interaction_part.shape)
+a.gen_norm_matrix()
 a.gen_random_response_var(3)
 a.naturalize()
 
 print("\nНормалізована матриця планування:")
-print(a.norm_matrix)
+print(a.main_part)
 
 print("\nВзаємодія:")
-print(a.interaction_part)
+print(numpy.array(a.interaction_part))
 
 print("\nКвадратична частина:")
 print(a.quadr_part)
+
+print("\nГотова матриця планування:")
+print(a.norm_matrix)
 
 print("Матриця функції відгуку")
 print(a.resp_var_matrix)
