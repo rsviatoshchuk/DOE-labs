c9174b5329c5ed19b1a94fe13de187d2dbf34c2f
Rostyslav Sviatoshchuk <svyatoschuk@gmail.com>
2020-04-07 19:55:06 +0300

    LAB 4 READY

--- autoexperiment.py
+++ autoexperiment.py
@@ -69,11 +69,38 @@ class Experiment:
         self.resp_var_matrix = matrix
         self.mean_resp_var_vector = self.resp_var_matrix.mean(axis=1)
 
-    def gen_random_response_var(self, m):
-        self.experiments = m
+    def reset_experiment(self):
+        self.factors = None
+        self.experiments = None
+        self.fractionality = None  # 0 for full factorial experiment
+        self.factors_ranges = None
+        self.resp_var_range = None
+        self.probability = None  # confidence probability
+        self.interaction_combinations = None
+        self.interaction_ranges = None
+        self.five_level_flag = None
+        self.interaction_flag = None
+        self.quadr_flag = None
+        self.main_part = None
+        self.interaction_part = None
+        self.quadr_part = None
+        self.norm_matrix = None
+        self.nat_main_part = None
+        self.nat_interaction_part = None
+        self.nat_quadr_part = None
+        self.nat_matrix = None
+        self.resp_var_matrix = None
+        self.mean_resp_var_vector = None
+        self.y = None
+        self.norm_regression_coef = None
+        self.nat_regression_coef = None
+        self.checked_nat_regr_coef = None
+        self.significant_coeffs = None
+
+    def gen_random_response_var(self):
         self.resp_var_matrix = numpy.random.uniform(low=self.resp_var_range[0],
                                                     high=self.resp_var_range[1],
-                                                    size=(len(self.main_part), m))
+                                                    size=(len(self.main_part), self.experiments))
         self.mean_resp_var_vector = self.resp_var_matrix.mean(axis=1)
 
     def gen_main_part(self):
@@ -150,14 +177,7 @@ class Experiment:
     def gen_norm_matrix(self):
 
         if self.interaction_flag:
-            if self.main_part.shape[0] == self.interaction_part.shape[0]:
-                matrix = numpy.append(self.main_part, self.interaction_part, axis=1)
-            else:
-                inter_matrix = numpy.append(self.interaction_part,
-                                            numpy.zeros((self.main_part.shape[0] - self.interaction_part.shape[0],
-                                                         self.interaction_part.shape[1])),
-                                            axis=0)
-                matrix = numpy.append(self.main_part, inter_matrix, axis=1)
+            matrix = numpy.append(self.main_part, self.interaction_part, axis=1)
             if self.quadr_flag:
                 self.norm_matrix = numpy.append(matrix, self.quadr_part, axis=1)
                 return
@@ -166,6 +186,8 @@ class Experiment:
                 return
         if self.quadr_flag:
             self.norm_matrix = numpy.append(self.main_part, self.quadr_part, axis=1)
+            return
+        self.norm_matrix = self.main_part[:, :]
 
     def gen_nat_matrix(self):
 
@@ -179,19 +201,33 @@ class Experiment:
                 return
         if self.quadr_flag:
             self.nat_matrix = numpy.append(self.nat_main_part, self.nat_quadr_part, axis=1)
+            return
+        self.nat_matrix = self.nat_main_part[:, :]
 
     def find_coef(self):
         norm_matr = numpy.append(numpy.ones((self.norm_matrix.shape[0], 1)), self.norm_matrix, axis=1)
-        self.norm_regression_coef = numpy.linalg.solve(norm_matr, self.mean_resp_var_vector)
+        if norm_matr.shape[0] != norm_matr.shape[1]:
+            norm_matr1 = norm_matr[1:norm_matr.shape[1]+1, :]
+            mean_resp_var_vector = self.mean_resp_var_vector[1:norm_matr.shape[1]+1]
+        else:
+            norm_matr1 = norm_matr
+            mean_resp_var_vector = self.mean_resp_var_vector
+        self.norm_regression_coef = numpy.linalg.solve(norm_matr1, mean_resp_var_vector)
 
-        yn = [sum(self.norm_regression_coef * norm_matr[i]) for i in range(self.norm_matrix.shape[0])]
-        print(yn)
+        # yn = [sum(self.norm_regression_coef * norm_matr[i]) for i in range(self.norm_matrix.shape[0])]
+        # print(yn)
 
-        nat_matr = numpy.append(numpy.ones((self.norm_matrix.shape[0], 1)), self.nat_matrix, axis=1)
-        self.nat_regression_coef = numpy.linalg.solve(nat_matr, self.mean_resp_var_vector)
+        nat_matr = numpy.append(numpy.ones((self.nat_matrix.shape[0], 1)), self.nat_matrix, axis=1)
+        if nat_matr.shape[0] != nat_matr.shape[1]:
+            nat_matr1 = nat_matr[1:nat_matr.shape[1]+1, :]
+            mean_resp_var_vector = self.mean_resp_var_vector[1:norm_matr.shape[1]+1]
+        else:
+            nat_matr1 = nat_matr
+            mean_resp_var_vector = self.mean_resp_var_vector
+        self.nat_regression_coef = numpy.linalg.solve(nat_matr1, mean_resp_var_vector)
 
-        yn = [sum(self.nat_regression_coef * nat_matr[i]) for i in range(self.nat_matrix.shape[0])]
-        print(yn)
+        # yn = [sum(self.nat_regression_coef * nat_matr[i]) for i in range(self.nat_matrix.shape[0])]
+        # print(yn)
 
     def cochran_test(self):
         variances = self.resp_var_matrix.var(axis=1)
@@ -204,8 +240,9 @@ class Experiment:
             return True
 
     def student_test(self):
-        variances = self.resp_var_matrix.var(axis=1)
-        s2_b = variances/(self.norm_matrix.shape[0]*self.experiments)
+        mean_variance = self.resp_var_matrix.var(axis=1).mean()
+        print(self.norm_matrix.shape[0]*self.experiments)
+        s2_b = mean_variance/(self.norm_matrix.shape[0]*self.experiments)
         t = abs(self.nat_regression_coef)/s2_b
 
         critical_student = self.get_student_critical(self.probability, (self.experiments-1) * self.norm_matrix.shape[0])
@@ -351,58 +388,100 @@ class Experiment:
     def get_l_rototable(k):
         return sqrt(k)
 
+    def lab4_start(self):
+        FLAG = True
+        while FLAG is True:
+            print("ПОЧАТОК\n")
+            self.set_experiment(3, [(-5, 15), (10, 60), (10, 20)], (205, 231.666))
+            self.experiments = 3
+            while True:
+                self.print_info()
+                self.gen_main_part()
+                if self.interaction_flag:
+                    self.gen_interaction_part()
+                self.gen_norm_matrix()
+                self.print_regression_eq()
+                self.gen_random_response_var()
+                self.print_norm_matrix()
+
+                self.naturalize()
+                self.gen_nat_matrix()
+                self.print_nat_matrix()
+                self.find_coef()
+                print(f"Нормалізовані коефіцієнти: {self.norm_regression_coef}")
+                print(f"Натуралізовані коефіцієнти: {self.nat_regression_coef}")
+
+                if self.cochran_test() is False:
+                    print("Дисперсії неоднорідні, збільшуємо m")
+                    self.experiments += 1
+                    continue
+                print("Дисперсії однорідні")
+                self.student_test()
+                print(f"Натуралізовані коефіцієнти: {self.checked_nat_regr_coef}")
+
+                if self.fisher_test():
+                    print("Адекватно")
+                    print(f"Натуралізовані коефіцієнти: {self.checked_nat_regr_coef}")
+                    FLAG = False
+                    break
+                else:
+                    print("Неадекватно")
+                    if self.interaction_flag is False:
+                        self.interaction_flag = True
+                        print("Додано ефект взаємодії")
+                        self.experiments = 3
+                    else:
+                        self.reset_experiment()
+                        break
+        print("Кінець")
+
+
 
 a = Experiment()
-a.set_experiment(3, [(-5, 15), (10, 60), (10, 20)], (205, 231.666), interaction=True, quadratic=False, fivelevel=False)
-a.gen_main_part()
-a.gen_interaction_part()
-a.gen_quadratic_part()
-a.gen_norm_matrix()
-a.gen_random_response_var(3)
-a.naturalize()
-a.gen_nat_matrix()
+# a.set_experiment(3, [(-5, 15), (10, 60), (10, 20)], (205, 231.666), interaction=True, quadratic=False, fivelevel=False)
+# a.gen_main_part()
+# a.gen_interaction_part()
+# a.gen_quadratic_part()
+# a.gen_norm_matrix()
+# a.gen_random_response_var(3)
+# a.naturalize()
+# a.gen_nat_matrix()
+#
+# print("\nМежі факторів:")
+# print(a.factors_ranges)
+#
+# print("\nНормалізована матриця планування:")
+# print(a.main_part)
+#
+# print("\nКомбінації взаємодії:")
+# print(a.interaction_combinations)
+#
+# print("\nМежі комбінацій взаємодії:")
+# print(a.interaction_ranges)
+#
+# print("\nВзаємодія:")
+# print(numpy.array(a.interaction_part))
+#
+# print("\nКвадратична частина:")
+# print(a.quadr_part)
+#
+# print("\nГотова матриця планування:")
+# print(a.norm_matrix)
+#
+# print("Матриця функції відгуку")
+# print(a.resp_var_matrix)
+#
+# print("\nНатуралізована матриця планування:")
+# print(a.nat_matrix)
+#
+# print("\nНатуралізована заємодія:")
+# print(a.nat_interaction_part)
+#
+# print("\nНатуралізована квадратична частина:")
+# print(a.nat_quadr_part)
+#
+# print("\nГотова натуралізована матриця планування:")
+# print(a.nat_matrix)
 
-print("\nМежі факторів:")
-print(a.factors_ranges)
 
-print("\nНормалізована матриця планування:")
-print(a.main_part)
-
-print("\nКомбінації взаємодії:")
-print(a.interaction_combinations)
-
-print("\nМежі комбінацій взаємодії:")
-print(a.interaction_ranges)
-
-print("\nВзаємодія:")
-print(numpy.array(a.interaction_part))
-
-print("\nКвадратична частина:")
-print(a.quadr_part)
-
-print("\nГотова матриця планування:")
-print(a.norm_matrix)
-
-print("Матриця функції відгуку")
-print(a.resp_var_matrix)
-
-print("\nНатуралізована матриця планування:")
-print(a.nat_matrix)
-
-print("\nНатуралізована заємодія:")
-print(a.nat_interaction_part)
-
-print("\nНатуралізована квадратична частина:")
-print(a.nat_quadr_part)
-
-print("\nГотова натуралізована матриця планування:")
-print(a.nat_matrix)
-
-a.print_info()
-a.print_norm_matrix()
-a.print_nat_matrix()
-a.print_regression_eq()
-a.cochran_test()
-a.find_coef()
-a.student_test()
-print(a.fisher_test())
+a.lab4_start()
